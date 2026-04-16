b997c6d29d5dbd3bc7220d7392c11f4603e65c95
chanyrubinsky <c0527601272@gmail.com>
2026-04-16 19:11:45 +0300

    finish the context

--- our-project/src/App.jsx
+++ our-project/src/App.jsx
@@ -1,51 +1,53 @@
-// // import { useState } from 'react'
-// // import { EditShbbatSettings } from './Components/home/EditShabbatSettings'
-// // import './App.css'
-// // import { ShopingList } from "./Components/shoping/ShopingList";
 
-// // import { StrictMode } from 'react'
-// // import { createRoot } from 'react-dom/client'
-// // // import{ListCooking} from './Components/Cooking/listCooking.jsx' 
-// // function App() {
-// //   return (
-// //     <>
-// //       <ShopingList/>
-// //        {/* <EditShbbatSettings />   */}
-// //          {/* <ListCooking />  */}
-// //          {/* <ListTask />  */}
-
-// //     </>
-// //   )
-// // }
-
-// // export default App
-// import { ShopingContextProvider } from "./shopingContext/shoping.context.jsx";
+ import { ShopingContextProvider } from "./shopingContext/shoping.context.jsx";
 // import { ShopingList } from "./components/ShopingList";
-
-// function App() {
-//   return (
-//     // ה-Provider חייב לעטוף את הכל!
-//     <ShopingContextProvider>
-//       <div className="App">
-//         <ShopingList isEditMode={true} />
-//       </div>
-//     </ShopingContextProvider>
-//   );
-// }
-import { ShopingContextProvider } from "./shopingContext/shoping.context.jsx";
-// וודאי שהנתיב כאן מדויק לקובץ שלך! 
-// אם הוא בתוך תיקיית components, רשמי כך:
 import ShoppingPage  from "./Components/shoping/ShoppingPage.jsx"; 
 
-function App() {
-  return (
-    <ShopingContextProvider>
-      <div className="App">
-        {/* העברתי לכאן את ה-isEditMode כדי שתוכלי לשלוט בזה */}
-        <ShoppingPage />
-      </div>
-    </ShopingContextProvider>
-  );
-}
+ function App() {
+return (
+//     // ה-Provider חייב לעטוף את הכל!
+ <ShopingContextProvider>
+       <div className="App">
+         <ShoppingPage />
+       </div>
+     </ShopingContextProvider>
+   );
+ }
+// import { ShopingContextProvider } from "./shopingContext/shoping.context.jsx";
+// // וודאי שהנתיב כאן מדויק לקובץ שלך! 
+// // אם הוא בתוך תיקיית components, רשמי כך:
+
+// function App() {
+
+//   return (
+//     <>
+//         {/* <Router>
+//             <Header />
+//             <Switch>
+//                 <Route path="/" exact component={ShabbatSettings} />
+//                 <Route path="/cookingList" component={ListCooking} />
+//                 <Route path="/tasksList" component={ListTask} />
+//                 <Route path="/ShopingList" component={ShopingList} />
+//                 הוספת רוטים נוספים
+//             </Switch>
+//         </Router> */}
+
+//         {/* <nav>
+//             <a href="/">הגדרות שבת</a>
+//             <a href="/ListTask">משימות</a>
+//             <a href="/ListCooking">בישולים</a>
+//             <a href="/ShopingList">קניות</a>
+//         </nav> */}
+//             {/* <ListCooking to="/ListCooking" end>בישולים</ListCooking>
+//             <ShopingList to="/ShopingList" end>קניות</ShopingList> */}
+//       <ShabbatProvider>
+//         <Header />
+//       <EditShabbatSettings />
+//         {/* <ShabbatSettings /> */}
+//       </ShabbatProvider>
+
+//     </>
+//   )
+// }
 
 export default App;
--- our-project/src/main.jsx
+++ our-project/src/main.jsx
@@ -1,14 +1,9 @@
-// import { createRoot } from "react-dom/client";
-// import App from './App.jsx';
-// import { ShopingList } from "./Components/shoping/ShopingList.jsx";
-
-// createRoot(document.getElementById('root')).render(
-//     <App />
-// );
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
-);
+   <StrictMode>
+    <App />,
+   </StrictMode>,
+)
